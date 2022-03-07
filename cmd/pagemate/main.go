7d3ac1dbfeb45e7251dbaa58f2b329a214801106
Jeffrey Buchbinder <jbuchbinder@ravemobilesafety.com>
2022-03-07 15:28:49 -0500

    Break out credentials, licensing

--- cmd/pagemate/main.go
+++ cmd/pagemate/main.go
@@ -1,3 +1,20 @@
+// Licensed to Dayville Fire Company under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Dayville Fire Company licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package main
 
 import (
@@ -29,7 +46,14 @@ func main() {
 		to = strings.TrimSpace(strings.ToUpper(env["TO"]))
 	}
 
-	pm := pagemate.NewPageMateClient(env["URL"], env["USER"], env["PASS"])
+	url := env["URL"]
+	if url == "" {
+		url = "http://pageme.qvec.org"
+	}
+	user := env["USER"]
+	pass := env["PASS"]
+
+	pm := pagemate.NewPageMateClient(url, user, pass)
 	groups, err := pm.FindRecipientGroups(to)
 	if err != nil {
 		panic(err)
